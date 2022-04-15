098866a06527df98be91457ecea33411a0d68f40
devnafiz <nafiz.2011@yahoo.com>
2022-04-16 03:16:38 +0600

    add question

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -65,18 +65,36 @@
 
                                 <div class="card-body">
                                     <form action="{{route('questions.store')}}" method="POST">
+                                        @csrf
                                         <div class="form-group">
                                             <label for="question-title">Question Title</label>
-                                            <input type="text" name="title" class="form-control" id="question-title" >
+                                            <input type="text" name="title" class="form-control {{$errors->has('title')? 'is-invalid' :''}}" id="question-title" >
+
+                                            @if($errors->has('title'))
+                                             <div class="invalid-feedback">
+                                                <strong>{{$errors->first('title')}}</strong>
+                                                 
+                                             </div>
+                                                }
+
+                                            @endif
                                             
                                         </div>
 
                                         <div class="form-group">
                                             <label for="question-body">Explain your question</label>
                                             
-                                            <textarea name="body" id="question-body" rows="10" class="form-control">
+                                            <textarea name="body" id="question-body" rows="10" class="form-control {{$errors->has('body')? 'is-invalid' :''}}">
                                                 
                                             </textarea>
+                                             @if($errors->has('body'))
+                                             <div class="invalid-feedback">
+                                                <strong>{{$errors->first('body')}}</strong>
+                                                 
+                                             </div>
+                                                }
+
+                                            @endif
                                             
                                         </div>
                                         <div class="form-group">
